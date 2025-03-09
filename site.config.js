80160de28b1928956aad22bd7cb849634e335971
lijunz56 <zlijun.engr@gmail.com>
2025-03-09 00:33:57 -0500

    push

--- site.config.js
+++ site.config.js
@@ -18,14 +18,14 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "uiw6unoh-log",
-    description: "welcome to uiw6unoh-log!",
+    title: "Lijourney",
+    description: "welcome to lijun's blog!",
   },
 
   // CONFIG configration (required)
-  link: "https://uiw6unoh-log.vercel.app",
+  link: "https://lijourney.vercel.app",
   since: 2024, // If leave this empty, current year will be used.
-  lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
